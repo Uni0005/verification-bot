d9c55073d65a41ac4d9e9afc230b2dc4b13c18d0
Uni <holandsol6@gmail.com>
2022-11-15 16:57:03 +0400

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Verification bot for PojavLauncher server
+## Is it safe?
+Yes, it is completely safe. You can see open source code of the bot in this repository. And also, if you are in doubt whether it's safe to log in to account, you can see permissions you grant when logging in to account
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
+const { Routes, ActivityType } = require('discord.js');
 const guildsDB = require('../util/models/guild');
 const mongoose = require('mongoose');
 
 module.exports = {
     name: 'ready', 
     async execute (client) {
-        client.user.setPresence({status: 'idle'});
         console.log(`Logged in as ${client.user.tag}!`);
-
-        if(process.env.MONGO_URL){
-            mongoose.connect(process.env.MONGO_URL, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }).then(() => console.log('Bot connected to database'));
-        };
-
+        client.user.setPresence({ activities: [{ name: 'minecraft.net', type: ActivityType.Watching }], status: 'idle' });
         await (client.channels.resolve('1001093728674525194')).send(`<@920325546200694905> restart?`);
+
+        if(!process.env.MONGO_URL) return;
+        
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => console.log('Bot connected to database'));
+
         const guilds = await guildsDB.find({slashCommands: true});
         if(!guilds) return;
         for(guild of guilds){
